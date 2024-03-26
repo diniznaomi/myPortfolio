bfcf59e712e4cf6b2415d89159ce93f825c31c6f
diniznaomi <nalindsaydm@gmail.com>
2024-03-26 19:05:35 -0300

    feat: adjust projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,15 +3,29 @@ import { Link as Link2 } from "react-router-dom";
 import * as Unicons from '@iconscout/react-unicons'
 import Lightbox from 'react-18-image-lightbox';
 import '../../node_modules/react-18-image-lightbox/style.css';
+import { Carousel } from "./Carousel";
+
 const images = [
-    "/images/works/1.jpg",
-    "/images/works/2.jpg",
-    "/images/works/3.jpg",
-    "/images/works/4.jpg",
-    "/images/works/5.jpg",
-    "/images/works/6.jpg",
-    "/images/works/7.jpg",
-    "/images/works/8.jpg",
+    "/images/works/TraveSon1.png",
+    "/images/works/TraveSon2.png",
+    "/images/works/TraveSon3.png",
+];
+
+const imagesDonto = [
+    "/images/works/Donto1.png",
+    "/images/works/Donto2.png",
+    "/images/works/Donto3.png",
+    "/images/works/Donto4.png",
+    "/images/works/Donto5.png",
+    "/images/works/Donto6.png",
+    "/images/works/Donto7.png",
+];
+
+let slides = [
+    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
+    "https://wallpapercave.com/wp/wp3386769.jpg",
+    "https://wallpaperaccess.com/full/809523.jpg",
+    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
 ];
 
 export default function Projects() {
@@ -27,68 +41,44 @@ export default function Projects() {
     return (
         <>
             <section className="relative md:py-24 dark:bg-slate-800 py-16" id="project">
+
                 <div className="container">
                     <div className="grid grid-cols-1 pb-8 text-center">
-                        <h3 className="mb-6 md:text-2xl text-xl md:leading-normal leading-normal font-semibold">My Work & Projects</h3>
+                        <h3 className="mb-6 md:text-2xl text-xl md:leading-normal leading-normal font-semibold">Alguns dos meus projetos...</h3>
 
-                        <p className="text-slate-400 max-w-xl mx-auto text-[15px]">Obviously I'm a Web Designer. Web Developer with over 7 years of experience. Experienced with all stages of the development.</p>
+                        {/* <p className="text-slate-400 max-w-xl mx-auto text-[15px]">Obviously I'm a Web Designer. Web Developer with over 7 years of experience. Experienced with all stages of the development.</p> */}
                     </div>
 
-                    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 mt-8 gap-[30px]">
+
+                    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 mt-8 gap-[30px] justify-center items-center">
                         <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/1.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
+                            <Carousel slides={images} />
                             <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Fronter Theme</Link2>
-                                <span className="block text-sm text-slate-400">UI / UX Design</span>
+                                <span className="block text-sm text-yellow-500">Hospedagem</span>
                             </div>
+                        </div>
+                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
+                          <Carousel slides={imagesDonto} />
+                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500 bg-gradient-to-b">
+                                <span className="block text-sm text-yellow-500">Clínica</span>
+                            </div>                            
+                        </div>
 
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-
-                                <Link2 to="#" onClick={() => handleCLick(0)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
+                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
+                         <Carousel slides={images} />
+                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
+                                <span className="block text-sm text-yellow-500">Landing</span>
                             </div>
                         </div>
 
                         <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/2.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
+                            <Carousel slides={images} />
                             <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Landrick Theme</Link2>
-                                <span className="block text-sm text-slate-400">Technology</span>
-                            </div>
-
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-                                <Link2 to="#" onClick={() => handleCLick(1)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
+                                <span className="block text-sm text-yellow-500">Landing</span>
                             </div>
                         </div>
 
-                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/3.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
-                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Valexy Theme</Link2>
-                                <span className="block text-sm text-slate-400">Landing</span>
-                            </div>
-
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-                                <Link2 to="#" onClick={() => handleCLick(2)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
-                            </div>
-                        </div>
-
-                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/4.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
-                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Superex Theme</Link2>
-                                <span className="block text-sm text-slate-400">Digital</span>
-                            </div>
-
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-                                <Link2 to="#" onClick={() => handleCLick(3)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
-                            </div>
-                        </div>
-
-                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
+                        {/* <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
                             <img src="/images/works/5.jpg" alt="" />
                             <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
                             <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
@@ -99,46 +89,8 @@ export default function Projects() {
                             <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
                                 <Link2 to="#" onClick={() => handleCLick(3)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
                             </div>
-                        </div>
-
-                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/6.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
-                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Giglink Theme</Link2>
-                                <span className="block text-sm text-slate-400">Digital</span>
-                            </div>
-
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-                                <Link2 to="#" onClick={() => handleCLick(5)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
-                            </div>
-                        </div>
-
-                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/7.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
-                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Upwind Theme</Link2>
-                                <span className="block text-sm text-slate-400">Landing</span>
-                            </div>
-
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-                                <Link2 to="#" onClick={() => handleCLick(6)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
-                            </div>
-                        </div>
-
-                        <div className="relative group overflow-hidden rounded-lg shadow shadow-slate-200 dark:shadow-gray-800">
-                            <img src="/images/works/8.jpg" alt="" />
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b to-slate-900 from-transparent transition-all duration-500"></div>
-                            <div className="absolute bottom-0 opacity-0 group-hover:opacity-100 m-6 transition-all duration-500">
-                                <Link2 to="#!" className="text-white hover:text-amber-500 font-semibold transition-all duration-500">Techwind Theme</Link2>
-                                <span className="block text-sm text-slate-400">Multipurpose</span>
-                            </div>
-
-                            <div className="absolute z-10 opacity-0 group-hover:opacity-100 top-1/2 -translate-y-1/2 right-0 left-0 mx-auto text-center transition-all duration-500">
-                                <Link2 to="#" onClick={() => handleCLick(7)} className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white btn-icon rounded-full lightbox"><Unicons.UilCamera width={16} /></Link2>
-                            </div>
-                        </div>
+                            </div>                    
+                        */}
                     </div>
                 </div>
             </section>
