26212eee03e2caef1558cd536d18a3a2a20a7011
diniznaomi <nalindsaydm@gmail.com>
2024-04-04 23:55:32 -0300

    feat: backend route

--- src/service.js
+++ src/service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const sendEmail = async(emailMessageData) =>{
     try{
-        const response = await axios.post('http://localhost:8000/send-email', emailMessageData);
+        const response = await axios.post('https://send-email-service-indol.vercel.app/send-email', emailMessageData);
         console.log('Response: ', response.data);
     }catch (error) {
         console.error(error);
